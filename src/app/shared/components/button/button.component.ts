98cd4551f6f7b2b71d5259febafbd40bd56c63c9
Marcelo Llerena RAVN <marcelo-ravn@Marcelo-Llerena-Ravn-GJQVR99KL6.local>
2026-07-20 19:03:22 -0500

    add button component with customs

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import { NgpButton } from 'ng-primitives/button';
+import { Component, input, OnInit } from '@angular/core';
+
+@Component({
+  imports: [NgpButton],
+  selector: 'button-component',
+  styleUrl: 'button.component.css',
+  templateUrl: 'button.component.html',
+})
+export class ButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  readonly size = input<ButtonSize>('md');
+  readonly type = input<ButtonType>('button');
+  readonly disabled = input<boolean>(false);
+  readonly ariaLabel = input<string | null>(null);
+}
