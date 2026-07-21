d7fa3788fa8da2f33104f4833aa1b28e24f3d0f8
Marcelo Llerena RAVN <marcelo-ravn@Marcelo-Llerena-Ravn-GJQVR99KL6.local>
2026-07-20 19:03:45 -0500

    add board component

---
+++ src/app/features/kanban/components/board/board.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'board-component',
+  templateUrl: 'board.component.html',
+  providers: [],
+})
+export class BoardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
