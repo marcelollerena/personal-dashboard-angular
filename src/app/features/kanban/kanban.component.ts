c5bbba82dd3aa2874d997ebb700f146cdd49fe85
Marcelo Llerena RAVN <marcelo-ravn@Marcelo-Llerena-Ravn-GJQVR99KL6.local>
2026-07-20 19:03:08 -0500

    add kanban page with layout

--- src/app/features/kanban/kanban.component.ts
+++ src/app/features/kanban/kanban.component.ts
@@ -1,8 +1,13 @@
+import { RouterOutlet } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+import { CreateTaskComponent } from './components/dialogs/create-task/create-task.component';
 
 @Component({
   selector: 'kanban-component',
+  styleUrl: 'kanban.component.css',
   templateUrl: 'kanban.component.html',
+  imports: [RouterOutlet, CreateTaskComponent],
 })
 export class KanbanComponent implements OnInit {
   constructor() {}
