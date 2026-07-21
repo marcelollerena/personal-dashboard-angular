a5a829fe10a2d679f794a570e289bca8c8d5956d
Marcelo Llerena RAVN <marcelo-ravn@Marcelo-Llerena-Ravn-GJQVR99KL6.local>
2026-07-20 19:04:12 -0500

    add create custom dialog component

---
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -0,0 +1,32 @@
+import { Component, input } from '@angular/core';
+
+import {
+  NgpDialog,
+  NgpDialogDescription,
+  NgpDialogOverlay,
+  NgpDialogTitle,
+  NgpDialogTrigger,
+} from 'ng-primitives/dialog';
+
+import { ButtonComponent } from '../button/button.component';
+import { provideIcons } from '@ng-icons/core';
+import { heroPlus } from '@ng-icons/heroicons/outline';
+
+@Component({
+  selector: 'dialog-component',
+  styleUrl: 'dialog.component.css',
+  templateUrl: 'dialog.component.html',
+  imports: [
+    NgpDialog,
+    NgpDialogOverlay,
+    NgpDialogTitle,
+    NgpDialogDescription,
+    NgpDialogTrigger,
+    ButtonComponent,
+  ],
+  providers: [provideIcons({ heroPlus })],
+})
+export default class DialogComponent {
+  readonly buttonTrigger = input('Open Dialog');
+  readonly dialogTitle = input('Dialog Title');
+}
